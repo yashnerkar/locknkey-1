a1f73dc37b4c0afc7e3031b10ae1f3c2f2ae0f85
Dhruv Rajput <raj.dhruv.777@gmail.com>
2022-09-15 11:14:15 +0530

    Update Questions

--- client/src/components/Questions.jsx
+++ client/src/components/Questions.jsx
@@ -2,61 +2,17 @@ import React from "react";
 
 const Question = ({ questions }) => {
   return (
-    <div
-      id="carouselExampleControls"
-      className="carousel mt-4"
-      //   data-bs-ride="carousel"
-    >
-      <div className="carousel-inner">
+    <div className="mt-4">
+      <div className="d-flex gap-4 justify-content-center">
         {questions &&
           questions.map((question, idx) => {
-            return idx == 0 ? (
-              <div className="carousel-item active" key={idx}>
-                <img
-                  className="key img-fluid"
-                  src={question}
-                  alt="..."
-                  style={{ height: "50%", width: "50%" }}
-                />
-              </div>
-            ) : (
-              <div className="carousel-item" key={idx}>
-                <img
-                  className="key"
-                  src={question}
-                  alt="..."
-                  style={{ height: "50%", width: "50%" }}
-                />
-              </div>
+            return (
+              <a href={question} className="btn btn-success px-4" target="_blank">
+                Question {idx+1}
+              </a>
             );
           })}
-        {/* <div className="carousel-item active">
-          <img className="key" src="images/pseudo_img.png" alt />
-        </div>
-        <div className="carousel-item">
-          <img className="key" src="images/pseudo_img.png" alt />
-        </div>
-        <div className="carousel-item">
-          <img lassName="key" src="images/pseudo_img.png" alt />
-        </div> */}
       </div>
-
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true" />
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true" />
-      </button>
     </div>
   );
 };
